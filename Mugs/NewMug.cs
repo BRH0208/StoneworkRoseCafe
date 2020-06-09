3aba6b1fd10e458fe0aee9dd9705a9d31cdb69ba
Ronan Finley <17411952+RonanFinley@users.noreply.github.com>
2020-06-08 22:56:51 -0600

    Revert "Cafe"

---
+++ Mugs/NewMug.cs
@@ -0,0 +1,12 @@
+﻿using Terraria.ModLoader;
+
+namespace StoneworkRoseCafe.Mugs {
+    class NewMug {
+		public string displayName;
+		public string texturePath;
+		public NewMug(string displayName, string texturePath) {
+			this.displayName = displayName;
+			this.texturePath = texturePath;
+		}
+	}
+}
