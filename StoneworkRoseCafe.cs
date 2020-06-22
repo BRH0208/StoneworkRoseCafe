80b525f3d845cf379f08c7917247f6e2ceff047b
BRH0208 <bryce.hawken@s.stemk12.org>
2020-06-21 22:49:33 -0600

    Update StoneworkRoseCafe.cs

--- StoneworkRoseCafe.cs
+++ StoneworkRoseCafe.cs
@@ -1,12 +1,30 @@
+using StoneworkRoseCafe;
 using StoneworkRoseCafe.Mugs;
 using StoneworkRoseCafe.Mugs.Tiles;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Terraria;
+using Terraria.GameContent.Dyes;
+using Terraria.GameContent.UI;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.Localization;
 using Terraria.ModLoader;
+using Terraria.UI;
+using static Terraria.ModLoader.ModContent;
 
 namespace StoneworkRoseCafe
 {
 	public class StoneworkRoseCafe : Mod
 	{
+		internal UserInterface HunterUserInterface;
+		
 		public StoneworkRoseCafe() {
 		}
 		private static NewMug[] mugs = new NewMug[] {
@@ -39,6 +57,26 @@ namespace StoneworkRoseCafe
 				MugIDs.Add(tile.Type); //add so we can get all the mugs later
 				AddItem(mymug.displayName, item);
 				tile.drop = item.item.type;
+			}
+			
+			HunterUserInterface = new UserInterface();
+		}
+		
+		public override void UpdateUI(GameTime gameTime) {
+			HunterUserInterface?.Update(gameTime); // I don't know why there is an '?' either, but without it, the code don't build
+		}
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers) {
+			int inventoryIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Inventory"));
+			if (inventoryIndex != -1) {
+				layers.Insert(inventoryIndex, new LegacyGameInterfaceLayer(
+					"ExampleMod: Example Person UI",
+					delegate {
+						// If the current UIState of the UserInterface is null, nothing will draw. We don't need to track a separate .visible value.
+						HunterUserInterface.Draw(Main.spriteBatch, new GameTime());
+						return true;
+					},
+					InterfaceScaleType.UI)
+				);
 			}
 		}
 	}
